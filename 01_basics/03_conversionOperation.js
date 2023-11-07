a65f80b83e365d43864755d8c324475fe4113120
Sahib Shad Jan <120084828+SahibJan07@users.noreply.github.com>
2023-11-07 19:36:57 +0000

    Conversion Of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score = "23abc";
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => string;
+// "33abc" => NaN;
+// true => 1; false => 0;
+
+let isLoggedIn = "moosa"
+let booleanIsLogedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLogedIn);
+
+// 1 => true; 0 => false 
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
