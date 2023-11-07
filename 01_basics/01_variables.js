cfd8d183f9b51182fa724dd34287839104b8fd24
Sahib Shad Jan <120084828+SahibJan07@users.noreply.github.com>
2023-11-07 16:54:11 +0000

    container_config and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 14453
+let accountEmail = "moosa0939@gmail.com";
+var accountPassword = "12345";
+accountCity = "karachi"
+
+// accountId = 2; // not Allowed 
+
+console.log(accountId);
