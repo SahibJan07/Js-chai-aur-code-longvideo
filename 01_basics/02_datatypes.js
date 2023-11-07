e877b459a5934da746fecc92606a804d31e3fcd7
Sahib Shad Jan <120084828+SahibJan07@users.noreply.github.com>
2023-11-07 18:11:14 +0000

    datatypes Done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,4 +1,16 @@
 "use strict";
 
 //alert(3 + 3); //we are using nodejs not browser 
+// code readibility should be high 
 
+// javasxript does not have too many data types 
+
+// number => 2 to power 53 
+// bigInt
+// string => '' ""
+// boolean => true false 
+// null => isky peche bht kahaniya hai yeh ek stand alone value hai || ek type hai 
+// undefined => jb apny value assign na kari ho srf variable declare kiya ho 
+// symbol => mainly use in React {unique}
+
+// object 
