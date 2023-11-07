635fe60693adb1bb1613224d12b860abbca7e44a
Sahib Shad Jan <120084828+SahibJan07@users.noreply.github.com>
2023-11-07 17:28:32 +0000

    learning Data Types

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,5 +4,13 @@ var accountPassword = "12345";
 accountCity = "karachi"
 
 // accountId = 2; // not Allowed 
+accountEmail = "atta@gmail.com";
+accountPassword = "456777node";
+accountCity = "Lahore";
 
 console.log(accountId);
+/*
+prefer not to use var 
+because of issue in block and functional scope 
+*/
+console.table([accountId, accountPassword, accountCity, accountEmail]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,4 @@
+"use strict";
+
+//alert(3 + 3); //we are using nodejs not browser 
+
