bdc5e409155d4aa2e00997aadc44f2624feaa7f4
Sahib Shad Jan <120084828+SahibJan07@users.noreply.github.com>
2023-11-15 19:35:22 +0000

    InterviewPrespective Notes

---
+++ 01_basics/04_dataSummary.js
@@ -0,0 +1,26 @@
+// this video is for datatypes summary to Interview perspective 
+// 1. Premitive datatypes :they are of the following 7 types 
+// 1. string 
+// 2.Number 
+// 3.boolean
+// 4.Null
+// 5.Undefined 
+// 6.Symbol // it is to a value as unique 
+// 7.Bigint
+
+// other we have refrence datatype we also call it as non-primitive
+// Array,Objects,Functions 
+
+//Javascript ka cousin bhai bi hai uska name hai typescript :)
+const score = 100 
+const scoreValue = 100.00
+// yeh dono number data mn hi ayenge kuyn k decimal lagany sy value change nhi hoti js mn 
+
+//const isLoggeedIn = false 
+//const outsideTemp = null
+
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
+// {this returned false because Symbol makes every variable as unique value}
